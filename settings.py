93a318951253aa9a20da2b177ae2ffe2fd329089
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-11-17 10:28:08 +0100

    test

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9pga^nez91^-ys@qdu(5!=u-ubagg!biv@2#0!xe_+380jpe=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
