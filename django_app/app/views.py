ccfe549263194828d42c1182d59fc499557d6027
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-05 13:36:29 +0100

    tt

--- django_app/app/views.py
+++ django_app/app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello_view(request):
-    return HttpResponse("Witaj w OKD")
+    return HttpResponse("Witaj w OKD 05.12.2023")
