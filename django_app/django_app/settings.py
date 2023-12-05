dec27b574fa31cf429943b9e991cc1fe53f203ce
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-05 12:55:47 +0100

    tt

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fd=v*c=op%k$x$-7byexd82y0mcharc(_2@4#tkos3zvow**e!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '192.168.199.41']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
