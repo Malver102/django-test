b257db3b027373ace11f139a22f754fe951a9e02
PAWEL SUSZKO <C:\Users\suspaw\AppData\Roaming\The Bat!>
2023-12-06 11:49:43 +0100

    rr

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -116,10 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), # Django will search for /src/
+]
+STATIC_ROOT = "static/"
+
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_ROOT = os.path.join("/var/www/django_app/static")
 
 
 # Default primary key field type
